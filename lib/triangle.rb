ad75b5a12fa93af36f859573b6616e933aa973de
Paul <pcherian10@gmail.com>
2018-05-14 00:54:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,11 +4,13 @@ class Triangle
 
   def initialize (attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
+    @dimension = []
     @dimensions << @length, @width, @height
   end
 
   def kind
-
+    :equilateral if @dimensions.all? {|side| @length}
+    :isosceles if @dimentions
 
   end
 end
