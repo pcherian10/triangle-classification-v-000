b7c60fc7e8e99a46a6a65e87d5c9f024cf0939cb
Paul <pcherian10@gmail.com>
2018-05-14 01:20:06 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,13 +4,16 @@ class Triangle
 
   def initialize (side_1, side_2, side_3)
     @sides = []
-    @sides << side_1, side2, side_3
+    @sides << [side_1, side2, side_3]
+    @sides.flatten
+  end
 
   def kind
    :scalene if @sides.uniq.length == 3
    :isosceles if @sides.uniq.length == 2
    :equilateral if @sides.uniq.length == 1
-     
+ end
+
 
 
 
