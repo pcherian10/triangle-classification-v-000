3c51407256b0c2a08f0fb54567d67d55acc17dec
Paul <pcherian10@gmail.com>
2018-05-14 00:48:51 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,15 @@
 class Triangle
 
-  attr_accessor :length, :width, :height
+  attr_accessor :length, :width, :height, :equilateral, :isosceles, :scalene
 
   def initialize (attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 
+  def kind
+    if
+
+  end
 end
 
 
