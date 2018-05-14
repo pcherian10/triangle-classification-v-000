c663a10f97b18160a6a9adc9582d07cfc0c85bd0
Paul <pcherian10@gmail.com>
2018-05-14 02:08:43 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,14 +14,17 @@ class Triangle
     while(i <= 1)
       j = i + 1
       while(j < @sides.length)
-        result << (@sides[i] + @sides[j])
+        result << (@sides[i] + @sides[j]) #[(0,1), (0,2), (1,2)]
         j += 1
       end
       i +=1
     end
-    result = result.reverse
-    if !(@sides.each.with_index {|side, idx| return false if side > result[idx]}) || (@sides.any? {|side| side <= 0})
-      begin
+    
+    result = result.reverse #[(1,2), (0,2), (0,1)]
+    if (@sides.each.with_index {|side, idx| return false if side > result[idx]}) || (@sides.any? {|side| side <= 0})
+      begin 
+        raise TriangleError
+      rescue
 
 
 
